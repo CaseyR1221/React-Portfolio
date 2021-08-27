18a933e14688e9ae5ead0d915502f40ca321108d
Casey R <crowlandsdev@gmail.com>
2021-08-27 09:51:26 -0400

    finished right side of resume page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,8 +4,9 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import me from '../images/me2.jpg'
+import codeSymbol from '../images/code-symbol.png'
 
-const About = () => {
+const About = ({ currentPage, handlePageChange }) => {
     return (
         <div className='about'>
             <div>
@@ -14,18 +15,72 @@ const About = () => {
             <Container>
                 <Row>
                     <Col className='about-me'>
-                        <p className='h4 text-center'>Hi, my name is Casey, and I am a hardworking young tech professional striving to acquire a comprehensive set of software development
-                     skills to broaden the range of projects that I may work on. I have always known that I wanted to make an impact in people lives and 
-                     that I wasn’t meant to just fade into the background. I aspire to become a competent and dependable mobile developer so that I may 
-                     contribute fully within my given field, and act as a fountain of information to those around me who are less knowledgeable.</p>
+                        <p className=''>Hi, my name’s Casey, and I’m a Full-Stack Developer who specializes in building and designing 
+                        exceptional and accessible digital experiences. I have always loved tech and building new things, so I am constantly reading 
+                        up on and experimenting with new technologies. I’ve worked with a wide variety of programming languages and frameworks, but 
+                        currently I use the MERN stack for most of my web development projects. I attended the University of Central Florida and completed 
+                        The UCF Full-Stack Web Development Bootcamp as well.
+                        </p>
+                        <p>If you every need something built of just want to chat, I’m your guy! Feel free to reach out to me on any of my social media 
+                        platforms listed at the bottom of the page or contact me via the "Contact" section of this site.
+                        </p>
                     </Col>
                 </Row>
+                <div>
+                    <Image src={codeSymbol} className='code-symbol'/>
+                </div>
                 <Row>
-                    <Col>
-                        <p>One</p>
+                    <Col className='sub-section pt-5'>
+                        <h3 className='sub-section-heading'>My Hobbies</h3>
+                        <ul>
+                            <li>Web Development</li>
+                            <li>Playing Disc Golf</li>
+                            <li>Playing Tennis</li>
+                            <li>Playing Basketball</li>
+                            <li>Video Games</li>
+                            <ul>
+                                <li>The Final Fantasy Series</li>
+                                <li>The Kingdom Hearts Series</li>
+                                <li>The Witcher Series</li>
+                            </ul>
+                            <li>Magic The Gathering</li>
+                            <li>Fishing</li>
+                        </ul>
                     </Col>
-                    <Col>
-                        <p>Two</p>
+                    <Col className='sub-section pt-5'>
+                        <h3 className='sub-section-heading'>My Proficiencies</h3>
+                        <ul>
+                            <li>Foundations</li>
+                            <ul>
+                                <li>MacOS</li>
+                                <li>HTML</li>
+                                <li>CSS</li>
+                                <li>Bootstrap</li>
+                                <li>JavaScript</li>
+                                <li>JQuery</li>
+                            </ul>
+                        </ul>
+                        <ul>
+                            <li>Front-end</li>
+                            <ul>
+                                <li>React.js</li>
+                                <li>Redux</li>
+                                <li>Vue.js</li>
+                                <li>Angular.js</li>
+                                <li>Netlify</li>
+                            </ul>
+                        </ul>
+                        <ul>
+                            <li>Back-end</li>
+                            <ul>
+                                <li>Node.js</li>
+                                <li>Express.js</li>
+                                <li>MongoDB</li>
+                                <li>MySQL</li>
+                                <li>PostgreSQL</li>
+                                <li>Heroku</li>
+                            </ul>   
+                        </ul>
                     </Col>
                 </Row>
             </Container>
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,11 +1,149 @@
-import React from 'react'
+import React from "react";
+import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Resume = () => {
-    return (
-        <div>
-            Hello 4
-        </div>
-    )
-}
+  return (
+    <div id="resume">
+      <Container>
+        <Row>
+          <Col className="resume-left">
+            <h1>HEllo</h1>
+          </Col>
+          <Col className="resume-right">
+            <div className="introduction pb-4">
+              <h4>Intro</h4>
+              <div className="content">
+                <p className="intro-p">
+                  I&#39;m a Full-Stack Developer who specializes in building and designing 
+                  exceptional and accessible digital experiences.
+                </p>
+                <p className="intro-p">
+                  I love designing and building new and exciting things, so I
+                  feel right at home as a Full-Stack Developer. I&#39;m
+                  experienced in developing and designing products for the web,
+                  from simple landing pages to full-fledged web apps.
+                </p>
+                <span className="intro-p job-pls">
+                  I&#39;m currently seeking a Front-End Development position 
+                  with an innovative company where i can put my creative skills to use.
+                </span>
+              </div>
+            </div>
+            <div className="skills pb-4">
+              <h4>Skills</h4>
+              <div className="content">
+                <div className="skill-category pb-2">
+                  <strong>Languages: &nbsp;</strong>
+                  <span>JavaScript (ES6), HTML5, CSS3, Java, PHP</span>
+                </div>
+                <div className="skill-category pb-2">
+                  <strong>Libraries &amp; Frameworks: &nbsp;</strong>
+                  <span>
+                    jQuery, Bootstrap, Bulma, Handlebars, Node, Express,
+                    MySQL, PostgreSQL, MongoDB, Angular, Vue, React, Redux
+                  </span>
+                </div>
+                <div className="skill-category">
+                  <strong>Tools: &nbsp;</strong>
+                  <span>
+                    Git &amp; Github, Command Line, Grunt, Insomnia, DBeaver,
+                     MongoDB Compass, Robo3T, Heroku, Netlify
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="education pb-4">
+              <h4>Education</h4>
+              <div className="content pb-3">
+                <div className="university">
+                    <strong>University of Central Florida</strong>
+                </div>
+                <div className="college pb-1">
+                    <em>College of Computer and Information Science</em>
+                </div>
+                <div className="major">
+                  <strong>Major:</strong> Computer Science
+                </div>
+              </div>
+              <div className="content">
+                <div className="university">
+                    <strong>University of Central Florida Coding Bootcamp</strong>
+                </div>
+                <div className="college pb-1">
+                    <em>College of Continuing Education</em>
+                </div>
+                <div className="major">
+                  <strong>Major:</strong> Full-Stack Web Development
+                </div>
+              </div>
+            </div>
+            <div className="projects">
+              <h4>Projects</h4>
+              <div className="content">
+                <div className="project-item">
+                  <a
+                    className="project-title"
+                    href=""
+                    target="_blank"
+                  >
+                    caseyrowlands.com
+                  </a>
+                  <p className="project-desc">
+                    Portfolio site designed and coded from scratch to showcase
+                    my skills and past work
+                  </p>
+                </div>
+                <div className="project-item">
+                  <div className="project-title">Tasty-Bytes</div>
+                  <p className="project-desc">
+                    Front end web app built using HTML, CSS/Bulma, and JavaScript
+                    to provide users a way to find recipes using food items they
+                    already have in their homes.
+                  </p>
+                </div>
+                <div className="project-item">
+                  <a
+                    className="project-title"
+                    href=""
+                    target="_blank"
+                  >
+                    Disc Jockies
+                  </a>
+                  <p className="project-desc">
+                    Full-stack Node.js application that uses a Handlebars.js view engine
+                    to render the front end. Allows users to view disc golf discs, leave
+                    reviews on discs, and fins disc gold courses near them.
+                  </p>
+                </div>
+                <div className="project-item">
+                  <a
+                    className="project-title"
+                    href=""
+                    target="_blank"
+                  >
+                    Note Taker
+                  </a>
+                  <p className="project-desc">
+                    Express.js, HTML, and CSS application that allows a user to write notes 
+                    that are saved to local storage, and tehn retrieve all of those notes to 
+                    be viewed at their convenience.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <div className="resume-PDF-button pt-3 pb-3">
+        <a href="" target="_blank">
+            <button type="button" class="btn btn-primary btn-lg">Grab a PDF of my full resume</button>
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default Resume
+export default Resume;
