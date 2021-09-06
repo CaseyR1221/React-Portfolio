b85bce88b6ed96b36f879c10c125e9982983bf65
Casey R <crowlandsdev@gmail.com>
2021-09-06 11:45:08 -0400

    test

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const hero = () => {
             <div class="jumbotron jumbotron-fluid">
                 <div class="container home-header">
                     <h1 class="display-3 jumbotron-header">Casey Rowlands</h1>
-                    <p class="lead">Hi! Welcome to my site. My name is Casey, and I'm a Web Developer specializing in the MERN stack.</p>
+                    <p class="lead">Hi! Welcome to my site. My name is Casey, and I'm a Web Developer specializing in the MERN stack!</p>
                 </div>
             </div>
                 <CarouselPage />
